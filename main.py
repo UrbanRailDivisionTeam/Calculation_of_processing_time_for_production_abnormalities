0322418185f691c0caae2c9fae0ee44a3552787a
cheakf <sunmoming#gmail.com>
2026-04-27 09:28:13 +0800

    init

---
+++ main.py
@@ -0,0 +1,196 @@
+import clickhouse_connect.driver.client
+import pandas as pd
+from datetime import datetime, timedelta
+import time_calc
+import warnings
+import clickhouse_connect
+from clickhouse_connect.driver import httputil
+
+warnings.simplefilter("ignore", FutureWarning)
+
+_client = None
+
+
+def get_client() -> clickhouse_connect.driver.client.Client:
+    global _client
+    if _client is None:
+        pool_mgr = httputil.get_pool_manager(maxsize=32, num_pools=2, block=True, timeout=300)
+        _client = clickhouse_connect.get_client(
+            host="10.24.5.59", port=8123, username="cheakf", password="Swq8855830.",
+            database="default", pool_mgr=pool_mgr
+        )
+    return _client
+
+
+def main():
+    local = get_client()
+
+    filter_time = pd.Timestamp("2025-1-6 00:00:00")
+    response_limit = timedelta(hours=2)  # 响应超时时间
+    handle_limit = timedelta(hours=8)  # 处理超时时间
+
+    def _is_workday(input_time: datetime):
+        dd = local.query_df(f"""
+                SELECT 
+                    bill.是否休息
+                FROM 
+                    (        
+                        SELECT
+                            *
+                        FROM
+                            ods.attendance_kq_scheduling_holiday
+                        WHERE
+                            Deleted = 0 QUALIFY row_number() OVER (
+                                PARTITION BY
+                                    zid
+                                ORDER BY
+                                    Version DESC
+                            ) = 1
+                    ) AS bill
+                WHERE bill."节假日日期" = '{input_time.strftime(r"%Y-%m-%d %H:%M")}'
+            """)
+        # 如果有额外的设定，就按照要求返回结果
+        if len(dd) > 0:
+            return not bool(dd[0])
+        # 如果没有额外的节假日，就按照周一到周五上班返回
+        return input_time.weekday() < 5
+    time_calc.is_workday = _is_workday
+
+    local.command("""
+        CREATE TABLE IF NOT EXISTS dwd.cg_mes_usm_exception_cache (
+            fzid VARCHAR PRIMARY KEY
+        )""")
+    cache_columns = ["fzid", "计算完成", "应响应时间", "响应是否超时", "应处理时间", "处理是否超时"]
+    exc_bill = local.query_df(f"""
+            SELECT 
+                bill.*, 
+                cache.*
+            FROM                     
+            (        
+                SELECT
+                    *
+                FROM
+                    ods.cg_mes_usm_exception_bill
+                WHERE
+                    "删除标识" = '未删除'
+                    AND Deleted = 0 QUALIFY row_number() OVER (
+                        PARTITION BY
+                            zid
+                        ORDER BY
+                            Version DESC
+                    ) = 1
+            ) AS bill
+            LEFT JOIN dwd.cg_mes_usm_exception_cache AS cache ON bill."zid" = cache."fzid"
+            WHERE bill."创建日期" > '{filter_time.strftime(r"%Y-%m-%d %H:%M")}'
+        """)
+    cols: list[list] = []
+    for _, row in exc_bill.iterrows():
+        if (not pd.isnull(row.get("计算完成"))) and bool(row.get("计算完成")) == True:
+            cols.append([
+                row[k] for k in cache_columns
+            ])
+            continue
+        request = pd.Timestamp(row["发起日期"]).to_pydatetime()
+        response_deadline = time_calc.worktime_add(request, response_limit)  # 响应最后期限
+        handle_deadline = time_calc.worktime_add(response_deadline, handle_limit)  # 处理最后期限
+        response_overtime = False  # 响应是否超时
+        if pd.isnull(row["响应日期"]):
+            response_overtime = datetime.now() > response_deadline
+        else:
+            response = pd.Timestamp(row["响应日期"]).to_pydatetime()
+            response_overtime = response > response_deadline
+            handle_deadline = time_calc.worktime_add(response, handle_limit)  # 响应后指定时间内处理
+        handle_overtime = False  # 处理是否超时
+        if pd.isnull(row["处理日期"]):
+            handle_overtime = datetime.now() > handle_deadline
+        else:
+            handle = pd.Timestamp(row["处理日期"]).to_pydatetime()
+            handle_overtime = handle > handle_deadline
+        finished = False
+        if row["异常状态分类"] == "待关闭" or row["异常状态分类"] == "已关闭":
+            finished = True
+        cols.append([str(row["zid"]),   finished,
+                     response_deadline, '否' if not response_overtime else '是',
+                     handle_deadline,   '否'if not handle_overtime else '是'
+                     ])
+
+    time_judge = pd.DataFrame(cols, columns=cache_columns)
+    local.command("""
+        CREATE OR REPLACE TABLE dwd.cg_mes_usm_exception_cache 
+        AS SELECT * FROM time_judge""")
+    local.command("""
+        CREATE OR REPLACE TABLE dwd.cg_mes_usm_exception_processed AS
+        WITH emp_org AS ( 
+            SELECT
+                emp."工号",
+                emp."姓名", 
+                org."组织名称" AS "组室",
+                org_p."组织名称" AS "部门"
+            FROM "ods"."person_employee" AS emp 
+            LEFT JOIN "ods"."person_organization" AS org ON emp."所属组织id" = org.zid
+            LEFT JOIN "ods"."person_organization" AS org_p ON org."上级组织id" = org_p.zid
+            WHERE
+                emp."任职状态" != '离职'
+                AND emp."任职状态" != '退休'
+                AND org."组织状态" = '启用'
+        ) SELECT
+            "bill"."zid",
+            "bill"."异常类型",
+            "bill"."异常类型编码",
+            "bill"."异常内容名称",
+            "bill"."异常描述",
+            "bill"."异常状态",
+            "bill"."项目",
+            "bill"."节车号",
+            "bill"."车号",
+            "bill"."工作中心",
+            "bill"."工位",
+            "bill"."工序名称",
+            "bill"."工序编码",
+            "bill"."异常内容描述",
+            "bill"."工位主键",
+            "bill"."发起人",
+            "bill"."发起日期",
+            "bill"."创建日期",
+            "bill"."修改日期",
+            "bill"."指定响应人",
+            "bill"."响应人",
+            "bill"."响应日期",
+            "bill"."处理人",
+            "bill"."处理日期",
+            "bill"."关闭人",
+            "bill"."关闭日期",
+            "bill"."异常状态分类",
+            "t"."应响应时间",
+            "t"."响应是否超时",
+            "t"."应处理时间",
+            "t"."处理是否超时",
+            "reporter"."姓名" AS "发起人姓名",
+            "reporter"."组室" AS "发起人组室",
+            "reporter"."部门" AS "发起人部门",
+            "appoint"."姓名" AS "指定响应人姓名",
+            "appoint"."组室" AS "指定响应人组室",
+            "appoint"."部门" AS "指定响应人部门",
+            "response"."姓名" AS "响应人姓名",
+            "response"."组室" AS "响应人组室",
+            "response"."部门" AS "响应人部门",
+            "handle"."姓名" AS "处理人姓名",
+            "handle"."组室" AS "处理人组室",
+            "handle"."部门" AS "处理人部门",
+            "closer"."姓名" AS "关闭人姓名",
+            "closer"."组室" AS "关闭人组室",
+            "closer"."部门" AS "关闭人部门",
+            "pro"."项目名称" AS "项目名称"
+        FROM "exc_bill" AS "bill"
+        LEFT JOIN "time_judge" AS "t" ON "bill"."zid" = "t"."fzid"
+        LEFT JOIN "emp_org" AS "reporter" ON "bill"."发起人" = "reporter"."工号"
+        LEFT JOIN "emp_org" AS "appoint" ON "bill"."指定响应人" = "appoint"."工号"
+        LEFT JOIN "emp_org" AS "response" ON "bill"."响应人"="response"."工号" 
+        LEFT JOIN "emp_org" AS "handle" ON "bill"."处理人"= "handle"."工号"
+        LEFT JOIN "emp_org" AS "closer" ON "bill"."关闭人"= "closer"."工号"
+        LEFT JOIN "ods"."crrc_project1" AS "pro" ON "bill"."项目" = "pro"."项目号"
+    """)
+
+
+if __name__ == "__main__":
+    main()
---
+++ time_calc.py
@@ -0,0 +1,105 @@
+from typing import Callable
+from dataclasses import dataclass
+from datetime import datetime,timedelta,date,time
+from pandas import Timestamp
+
+@dataclass
+class Interval():
+    left:timedelta
+    right:timedelta
+    @property
+    def length(self):
+        return self.right-self.left
+
+
+worktime = [
+    Interval(timedelta(hours=8, minutes=30, seconds=0),timedelta(hours=12, minutes=0, seconds=0)),
+    Interval(timedelta(hours=13, minutes=30, seconds=0),timedelta(hours=17, minutes=30, seconds=0))
+]
+'''一天内工作时间,0点开始,按小时记'''
+
+is_workday: Callable[[datetime], bool] = lambda date: date.weekday() < 5
+
+
+def real2work_hour(time_input: timedelta) -> timedelta:
+    # assert(time >= timedelta(minutes=0) and time <= timedelta(hours=24))
+    # 如果输入大于24小时,则输出为一天最大工作时间
+    t = timedelta(microseconds=0)
+    for i in worktime:
+        if time_input > i.right:
+            t += i.length
+            continue
+        elif time_input < i.left:
+            continue
+        t += time_input-i.left
+    return t
+
+
+def work2real_hour(duration: timedelta) -> timedelta:
+    assert (duration >= timedelta(minutes=0) and
+            duration <= real2work_hour(timedelta(hours=24)))
+    t = duration
+    i: Interval=Interval(timedelta(hours=0),timedelta(hours=0))
+    for i in worktime:
+        if t <= i.length:
+            break
+        t -= i.length
+    return i.left + t
+
+
+def real2work(time_input: datetime, base_time:datetime):
+    t = base_time
+    duration = timedelta(seconds=0)
+    while (t < time_input):
+        if is_workday(t):
+            duration += real2work_hour(time_input-t)
+        t += timedelta(days=1)
+    return duration
+
+
+def work2real(duration: timedelta, base_time:datetime):
+    t = base_time
+    d = duration
+    worktime_fullday = real2work_hour(timedelta(hours=24))
+    while (d > timedelta(seconds=0)):
+        if is_workday(t):
+            if d < worktime_fullday:
+                break
+            d -= worktime_fullday
+        t += timedelta(days=1)
+    return t + work2real_hour(d)
+
+
+def worktime_add(time_input: datetime, duration: timedelta):
+    '''
+    使用输入时间向前取整作为基准时间
+    '''
+    base_time=datetime.combine( 
+        date=time_input.date(),
+        time=time(hour=0,minute=0,microsecond=0)
+        )
+    return work2real(
+        real2work(time_input,base_time)+duration,
+        base_time)
+
+
+# def worktime_sub(time_input: datetime, timeOrDuration: timedelta | datetime):
+#     '''
+#     # 该函数有问题,暂时不要用
+#     '''
+#     if isinstance(timeOrDuration, timedelta):
+#         base_time=datetime.combine( 
+#             date=time_input.date(),
+#             time=time(hour=0,minute=0,microsecond=0)
+#         )
+#         return work2real(
+#             real2work(time_input,base_time)-timeOrDuration,
+#             base_time)
+#     elif isinstance(timeOrDuration, datetime):
+#         base_time=datetime.combine( 
+#             date=timeOrDuration.date(),
+#             time=time(hour=0,minute=0,microsecond=0)
+#         )
+#         return real2work(time_input)-real2work(timeOrDuration)
+#     else:
+#         raise TypeError(timeOrDuration)
